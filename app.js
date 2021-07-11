7508c5b03b12d87470af5cbfe52531311559bf3c
Parth-s28 <62176805+Parth-s28@users.noreply.github.com>
2021-07-11 13:11:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 //app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true})); 
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/bot-ui-master'));
 
